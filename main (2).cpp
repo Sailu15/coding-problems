81e7e11624bbbc9743de60d66e027af4f878ab05
Sailendiran.R <91106414+Sailu15@users.noreply.github.com>
2023-08-12 10:56:50 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){ 
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
